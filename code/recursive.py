76b3f8c725a1563051411fda89784b01773a73ca
ricguan1 <rguan1@babson.edu>
2026-02-03 11:13:56 -0500

    add s05.py in class work

---
+++ code/recursive.py
@@ -0,0 +1,12 @@
+import time
+
+
+def groundhog_day():
+    """A function that prints 'groundhod day' every second indefinetly"""
+    print("did you mean groundhog day")
+
+    time.sleep(1)
+    groundhog_day()
+
+
+    groundhog_day()
---
+++ code/s05.py
@@ -0,0 +1,10 @@
+a = int(input("Enter an integer:"))
+a = int(a)
+print(type(a))
+
+# check odd or even
+if a % 2 == 0:
+    print("Even")
+
+else:
+    print("Odd")
