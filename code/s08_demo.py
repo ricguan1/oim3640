ef46801ac940b9fd4c860d0486f59cf0398339a1
ricguan1 <rguan1@babson.edu>
2026-02-19 01:48:00 -0500

    complete chapter 5 content

---
+++ code/s08_demo.py
@@ -0,0 +1,17 @@
+# age = int(input("What is your age?  >>"))
+# if age < 18:
+#     print(teenager)
+# else:
+#     print(adult)
+
+
+score = 91
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("F")
