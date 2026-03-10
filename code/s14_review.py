05c914edab2617ab8b4a2b47eea36fdb23278948
ricguan1 <rguan1@babson.edu>
2026-03-10 10:52:25 -0400

    Create s14_review.py

---
+++ code/s14_review.py
@@ -0,0 +1,14 @@
+# a = [1, 2, 3]
+# b = a
+# b.append(4)
+# print(a)
+# print(a is b)
+
+# a = [1, 2, 3]
+# b = a[:]
+# b.append(4)
+# print(a)
+# print(a is b)
+
+eng2esp = {"one": "uno", "two": "dos", "three": "tres"}
+print(eng2esp["one"])
